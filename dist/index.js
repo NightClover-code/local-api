ac23774dadc5330f07f429d42fbc2ac52aaf0703
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-27 15:08:46 +0100

    adding a proxy

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-//importing express
+//importing express & proxy
 var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 //serve
 var serve = function (port, filename, dir) {
     var app = express_1.default();
+    //adding a proxy
+    app.use(http_proxy_middleware_1.createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+    }));
     //wrapping express into a promise
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
